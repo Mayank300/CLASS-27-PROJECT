062b8ddb7b12e14ad8c6efbd329f5e78639714a9
Mayank300 <70268410+Mayank300@users.noreply.github.com>
2020-09-17 12:32:42 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,27 @@
+class Bob{
+    constructor(x, y, radius, options) {
+        var options = {
+            isStatic    : false, 
+            restitution : 0.5,
+            friction    : 0.5,
+            density     : 1.2
+        }
+        this.body = Bodies.circle(x, y, radius, options);
+        this.radius = radius;
+         
+        World.add(world,this.body);
+      }
+      display(){
+        var pos =this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        ellipseMode(RADIUS);
+        strokeWeight(4);
+        stroke("black")
+        fill("yellow");
+        ellipse(0,0,this.radius/2,this.radius/2);
+        pop();
+      } 
+}
